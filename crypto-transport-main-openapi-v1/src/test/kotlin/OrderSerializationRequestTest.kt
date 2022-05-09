f5af65c61083fc18005d833fa79125b13e7916cc
schnaps <ulyanov@amediateka.ru>
2022-05-10 02:38:17 +0700

    добавлен UserId в запросы, обрабатывающие ордера рефакторинг тестов в связи с вышесказанным

--- crypto-transport-main-openapi-v1/src/test/kotlin/OrderSerializationRequestTest.kt
+++ crypto-transport-main-openapi-v1/src/test/kotlin/OrderSerializationRequestTest.kt
@@ -11,7 +11,8 @@ class OrderSerializationRequestTest {
             pair = TickerPair(first = "BTC", second = "USD"),
             orderType = OrderType.BUY,
             price = 100.0,
-            quantity = 1.0
+            quantity = 1.0,
+            userId = "user_123"
         )
 
         val jsonString = apiV1RequestSerialize(request)
@@ -23,12 +24,13 @@ class OrderSerializationRequestTest {
         assertContains(jsonString, "\"quantity\":1.0")
         assertContains(jsonString, "\"price\":100.0")
         assertContains(jsonString, "\"orderType\":\"buy\"")
+        assertContains(jsonString, "\"userId\":\"user_123\"")
     }
 
     @Test
     fun `deserialize order create request test`() {
         val response =
-            "{\"requestType\":\"OrderCreate\",\"requestId\":null,\"pair\":{\"first\":\"BTC\",\"second\":\"USD\"},\"quantity\":1.0,\"price\":100.0,\"orderType\":\"buy\"}"
+            "{\"requestType\":\"OrderCreate\",\"requestId\":null,\"pair\":{\"first\":\"BTC\",\"second\":\"USD\"},\"quantity\":1.0,\"price\":100.0,\"orderType\":\"buy\",\"userId\":\"user_123\"}"
 
         val decoded = apiV1RequestDeserialize<OrderCreateRequest>(response)
 
@@ -40,11 +42,12 @@ class OrderSerializationRequestTest {
         assertEquals(1.0, decoded.quantity)
         assertEquals(100.0, decoded.price)
         assertEquals(OrderType.BUY, decoded.orderType)
+        assertEquals("user_123", decoded.userId)
     }
 
     @Test
     fun `serialize order delete request test`() {
-        val request = OrderDeleteRequest(orderId = "idOrder")
+        val request = OrderDeleteRequest(orderId = "idOrder", userId = "user_123")
 
         val jsonString = apiV1RequestSerialize(request)
 
@@ -52,11 +55,12 @@ class OrderSerializationRequestTest {
 
         assertContains(jsonString, "\"requestType\":\"OrderDelete\"")
         assertContains(jsonString, "\"orderId\":\"idOrder\"")
+        assertContains(jsonString, "\"userId\":\"user_123\"")
     }
 
     @Test
     fun `deserialize order delete request test`() {
-        val response = "{\"requestType\":\"OrderDelete\",\"requestId\":null,\"orderId\":\"idOrder\"}"
+        val response = "{\"requestType\":\"OrderDelete\",\"requestId\":null,\"orderId\":\"idOrder\",\"userId\":\"user_123\"}"
 
         val decoded = apiV1RequestDeserialize<OrderDeleteRequest>(response)
 
@@ -64,22 +68,24 @@ class OrderSerializationRequestTest {
 
         assertEquals("OrderDelete", decoded.requestType)
         assertEquals("idOrder", decoded.orderId)
+        assertEquals("user_123", decoded.userId)
     }
 
     @Test
     fun `serialize order read request with no filter`() {
-        val request = OrderReadRequest()
+        val request = OrderReadRequest(userId = "user_123")
 
         val jsonString = apiV1RequestSerialize(request)
 
         println(jsonString)
         assertContains(jsonString, "\"requestType\":\"OrderRead\"")
+        assertContains(jsonString, "\"userId\":\"user_123\"")
         assertContains(jsonString, "\"filter\":null")
     }
 
     @Test
     fun `deserialize order read request with no filter`() {
-        val response = "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":null}"
+        val response = "{\"requestType\":\"OrderRead\",\"requestId\":null,\"userId\":\"user_123\",\"filter\":null}"
 
         val decoded = apiV1RequestDeserialize<OrderReadRequest>(response)
 
@@ -87,18 +93,21 @@ class OrderSerializationRequestTest {
 
         assertEquals("OrderRead", decoded.requestType)
         assertEquals(null, decoded.filter)
+        assertEquals("user_123", decoded.userId)
     }
 
     @Test
     fun `serialize order read request with filter by date`() {
         val request = OrderReadRequest(
-            filter = FilterByDate(date = "11.11.11")
+            filter = FilterByDate(date = "11.11.11"),
+            userId = "user_123"
         )
 
         val jsonString = apiV1RequestSerialize(request)
 
         println(jsonString)
         assertContains(jsonString, "\"requestType\":\"OrderRead\"")
+        assertContains(jsonString, "\"userId\":\"user_123\"")
         assertContains(jsonString, "\"filterType\":\"byDate\"")
         assertContains(jsonString, "\"date\":\"11.11.11\"")
     }
@@ -106,7 +115,7 @@ class OrderSerializationRequestTest {
     @Test
     fun `deserialize order read request with filter by date`() {
         val response =
-            "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":{\"filterType\":\"byDate\",\"date\":\"11.11.11\"}}"
+            "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":{\"filterType\":\"byDate\",\"date\":\"11.11.11\"},\"userId\":\"user_123\"}"
 
         val decoded = apiV1RequestDeserialize<OrderReadRequest>(response)
 
@@ -115,13 +124,15 @@ class OrderSerializationRequestTest {
         assertEquals("OrderRead", decoded.requestType)
         assert(decoded.filter is FilterByDate)
         assertEquals("byDate", decoded.filter?.filterType)
+        assertEquals("user_123", decoded.userId)
         assertEquals("11.11.11", (decoded.filter as? FilterByDate)?.date)
     }
 
     @Test
     fun `serialize order read request with filter by state`() {
         val request = OrderReadRequest(
-            filter = FilterByState(state = OrderState.ACTIVE)
+            filter = FilterByState(state = OrderState.ACTIVE),
+            userId = "user_123"
         )
 
         val jsonString = apiV1RequestSerialize(request)
@@ -129,13 +140,14 @@ class OrderSerializationRequestTest {
         println(jsonString)
         assertContains(jsonString, "\"requestType\":\"OrderRead\"")
         assertContains(jsonString, "\"filterType\":\"byState\"")
+        assertContains(jsonString, "\"userId\":\"user_123\"")
         assertContains(jsonString, "\"state\":\"active\"")
     }
 
     @Test
     fun `deserialize order read request with filter by state`() {
         val response =
-            "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":{\"filterType\":\"byState\",\"state\":\"active\"}}"
+            "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":{\"filterType\":\"byState\",\"state\":\"active\"},\"userId\":\"user_123\"}"
 
         val decoded = apiV1RequestDeserialize<OrderReadRequest>(response)
 
@@ -144,13 +156,15 @@ class OrderSerializationRequestTest {
         assertEquals("OrderRead", decoded.requestType)
         assert(decoded.filter is FilterByState)
         assertEquals("byState", decoded.filter?.filterType)
+        assertEquals("user_123", decoded.userId)
         assertEquals(OrderState.ACTIVE, (decoded.filter as? FilterByState)?.state)
     }
 
     @Test
     fun `serialize order read request with filter by type`() {
         val request = OrderReadRequest(
-            filter = FilterByType(type = OrderType.BUY)
+            filter = FilterByType(type = OrderType.BUY),
+            userId = "user_123"
         )
 
         val jsonString = apiV1RequestSerialize(request)
@@ -158,13 +172,14 @@ class OrderSerializationRequestTest {
         println(jsonString)
         assertContains(jsonString, "\"requestType\":\"OrderRead\"")
         assertContains(jsonString, "\"filterType\":\"byType\"")
+        assertContains(jsonString, "\"userId\":\"user_123\"")
         assertContains(jsonString, "\"type\":\"buy\"")
     }
 
     @Test
     fun `deserialize order read request with filter by type`() {
         val response =
-            "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":{\"filterType\":\"byType\",\"type\":\"buy\"}}"
+            "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":{\"filterType\":\"byType\",\"type\":\"buy\"},\"userId\":\"user_123\"}"
 
         val decoded = apiV1RequestDeserialize<OrderReadRequest>(response)
 
@@ -173,13 +188,15 @@ class OrderSerializationRequestTest {
         assertEquals("OrderRead", decoded.requestType)
         assert(decoded.filter is FilterByType)
         assertEquals("byType", decoded.filter?.filterType)
+        assertEquals("user_123", decoded.userId)
         assertEquals(OrderType.BUY, (decoded.filter as? FilterByType)?.type)
     }
 
     @Test
     fun `serialize order read request with filter by currency`() {
         val request = OrderReadRequest(
-            filter = FilterByCurrency(currency = "BTC")
+            filter = FilterByCurrency(currency = "BTC"),
+            userId = "user_123"
         )
 
         val jsonString = apiV1RequestSerialize(request)
@@ -187,13 +204,14 @@ class OrderSerializationRequestTest {
         println(jsonString)
         assertContains(jsonString, "\"requestType\":\"OrderRead\"")
         assertContains(jsonString, "\"filterType\":\"byCurrency\"")
+        assertContains(jsonString, "\"userId\":\"user_123\"")
         assertContains(jsonString, "\"currency\":\"BTC\"")
     }
 
     @Test
     fun `deserialize order read request with filter by currency`() {
         val response =
-            "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":{\"filterType\":\"byCurrency\",\"currency\":\"BTC\"}}"
+            "{\"requestType\":\"OrderRead\",\"requestId\":null,\"filter\":{\"filterType\":\"byCurrency\",\"currency\":\"BTC\"},\"userId\":\"user_123\"}"
 
         val decoded = apiV1RequestDeserialize<OrderReadRequest>(response)
 
@@ -202,6 +220,7 @@ class OrderSerializationRequestTest {
         assertEquals("OrderRead", decoded.requestType)
         assert(decoded.filter is FilterByCurrency)
         assertEquals("byCurrency", decoded.filter?.filterType)
+        assertEquals("user_123", decoded.userId)
         assertEquals("BTC", (decoded.filter as? FilterByCurrency?)?.currency)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,10 @@ pluginManagement {
         val openapiVersion: String by settings
 
         kotlin("jvm") version kotlinVersion apply false
+        kotlin("multiplatfrom") version kotlinVersion apply false
 
         id("org.openapi.generator") version openapiVersion apply false
     }
 }
-include("transport-main-openapi-v1")
+include("crypto-transport-main-openapi-v1")
+include("crypto-common")
